c1ceff49068d9e5c53f1bc4c3f99cf1f76b6875b
Anshul Nema <156672450+Anshul2308z@users.noreply.github.com>
2025-02-04 16:17:41 +0530

    Update 1.kt

--- Basic problems/1.kt
+++ Basic problems/1.kt
@@ -1,1 +1,12 @@
+fun main(){
+    val a = 450 // today's screentime
+    val b = 370 // yesterday's screentime
+    val info = screentime(a,b)
+    println("Your screentime today is more compared to yesterday: $info")
+}
+
+fun screentime(a: Int, b: Int):String{
+    val c: Boolean = a > b ;
+    return "$c" 
+}
 
