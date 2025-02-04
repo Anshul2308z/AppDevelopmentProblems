2e18235c80ab6635a50160872cb2e5020a8629ee
Anshul Nema <156672450+Anshul2308z@users.noreply.github.com>
2025-02-04 17:01:06 +0530

    Update sol02.kt

--- Basic problems/sol02.kt
+++ Basic problems/sol02.kt
@@ -20,3 +20,18 @@ fun sorter(city: String, lowTemp: Int, highTemp: Int, probabilityOfRain: Int) :S
     return ("City: $city\n Low temperature: $lowTemp, High temperature: $highTemp\n Chance of rain: $probabilityOfRain%")
     
 }
+/* original solution
+fun main() {
+    printWeatherForCity("Ankara", 27, 31, 82)
+    printWeatherForCity("Tokyo", 32, 36, 10)
+    printWeatherForCity("Cape Town", 59, 64, 2)
+    printWeatherForCity("Guatemala City", 50, 55, 7)
+}
+
+fun printWeatherForCity(cityName: String, lowTemp: Int, highTemp: Int, chanceOfRain: Int) {
+    println("City: $cityName")
+    println("Low temperature: $lowTemp, High temperature: $highTemp")
+    println("Chance of rain: $chanceOfRain%")
+    println()
+}
+*/
